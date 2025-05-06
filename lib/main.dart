d589f107fc2e42c501883d0e43b8e320b04480a0
ahmadabood97 <1997ahmadabood1997@gmail.com>
2025-05-06 19:16:00 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ScheduleApp());
+}
+
+class ScheduleApp extends StatelessWidget {
+  const ScheduleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('تطبيق المواعيد'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+    );
+  }
+}
